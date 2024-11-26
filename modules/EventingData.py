3a18f58a099c9d167a2b0931283cd261adb27b8a
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-11-26 00:54:16 -0500

    Create EventingData.py

---
+++ modules/EventingData.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def Samsung():
+    st.header('Samsung page')
+
+    st.markdown(
+        '''
+        **Samsung is a South Korean multinational conglomerate** known for its diverse range
+        of products and services, including consumer electronics, semiconductors, home appliances,
+        and telecommunications equipment. It's renowned for its innovative smartphones, TVs, and display technology.   
+        '''
+    )
