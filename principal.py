cc2c8e6c4aa85ad16928ceefbd0f52a937d745b7
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-12-21 19:16:55 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 def Home():
     st.header('WIN STATS')
-    st.markdown('''A streamlit application that uses antd components.''')
+    st.markdown('''A streamlit applicaDSAtion that uses antd components.''')
 
 def redirect_to_url(url):
     """Redirecciona automáticamente a una URL."""
